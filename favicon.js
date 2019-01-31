7b7002dc906991e26c39a0ff7cd09db9a5b09110
LunarTiger <lunartiger@live.com>
2019-01-31 11:35:32 -0500

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -1,4 +1,4 @@
-document.body.insertAdjacentHTML('afterbegin',`<p style="font-size:65%;color:#333;">&#9913;mirror&#9913;</p>`)
+document.body.insertAdjacentHTML('afterbegin',`<p style="font-size:65%;color:#555;">&#9913;mirror&#9913;</p>`);
 document.head.insertAdjacentHTML('beforeend',`<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=RyQA5rLO48">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=RyQA5rLO48">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=RyQA5rLO48">
