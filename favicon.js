e0a0cf8ddc140f6a6fca7382e270ee6d9d13d6a0
LunarTiger <lunartiger@live.com>
2019-01-31 11:21:49 -0500

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -1,7 +1,8 @@
+document.body.insertAdjacentHTML('afterbegin',`<p style="font-size:65%;color:#333;">&#9913;mirror&#9913;</p>`)
 document.head.insertAdjacentHTML('beforeend',`<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=RyQA5rLO48">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=RyQA5rLO48">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=RyQA5rLO48">
-<link rel="manifest" href="/site.webmanifest?v=RyQA5rLO48">
+<link rel="manifest" href="/fs-mirror/site.webmanifest?v=RyQA5rLO48">
 <link rel="mask-icon" href="/safari-pinned-tab.svg?v=RyQA5rLO48" color="#006900">
 <link rel="shortcut icon" href="/favicon.ico?v=RyQA5rLO48">
 <meta name="msapplication-TileColor" content="#00a300">
