20fb37edb768f8f3f94f18f41ddf4c1e9a562de5
LunarTiger <lunartiger@live.com>
2019-02-07 15:31:58 -0500

    Update collapsible.js

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -9,11 +9,12 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 var default_open = document.getElementById('openme').click();
-if (default_open != null && typeof default_open != undefined) {default_open = false;}
+var can_open = true;
+if (default_open != null && typeof default_open != undefined) {can_open = false;}
 if(window.location.hash){
 	var director = document.getElementById(window.location.hash.substr(1));
 	if (director != null && typeof director != undefined) {
 		var clickButton = document.getElementById(director.dataset.parent);
 		if (clickButton != null && typeof clickButton != undefined) {clickButton.click();}
-	} else {if (default_open) {default_open.click();}}
-} else {if (default_open) {default_open.click();}}
+	} else {if (can_open) {default_open.click();}}
+} else {if (can_open) {default_open.click();}}
