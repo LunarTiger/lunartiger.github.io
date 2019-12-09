61bb7a0af68aa900da350abc67ca4b4fb891345a
LunarTiger <lunartiger@live.com>
2019-12-09 02:00:02 -0500

    :tada:

--- assets/js/music-embed.js
+++ assets/js/music-embed.js
@@ -5,9 +5,13 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var state = database.ref('state');
 state.on('value', (function(snapshot) {
+	var stateVal = snapshot.val();
 	var id = database.ref('id');
 	id.on('value', (function(snapshot) {
 		var idVal = snapshot.val();
-		document.getElementById('musicembed').src = "https://www.youtube.com/embed/"+idVal+"?rel=0;&autoplay=1";
+		if(stateVal){
+			document.getElementById('musicembed').style = "max-width:100%;height:30px;width:350px;border: 0px"
+			document.getElementById('musicembed').src = "https://www.youtube.com/embed/"+idVal+"?rel=0;&autoplay=1";
+		}
 	}));
 }));
