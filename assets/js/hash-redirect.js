fcf46ce63892fd8091aa1ad5d5e80cc07d0a0a75
LunarTiger <lunartiger@live.com>
2019-12-09 04:37:34 -0500

    because efficiency

--- assets/js/hash-redirect.js
+++ assets/js/hash-redirect.js
@@ -1,3 +1,7 @@
-if(window.location.hash=="#files"){window.location='http://lunar.zapto.org';}
-else if(window.location.hash=="#world"){window.location='https://account.altvr.com/worlds/954689156213113037';}
-else if(window.location.hash){window.location=window.location.origin+'/'+window.location.hash.substr(1);}
+if(window.location.hash){
+	var goto = window.location.hash.substr(1);
+	if(goto=="files"){window.location=window.location.origin+'/fs-up';}
+	else if(goto=="lunarverse"){window.location='https://account.altvr.com/worlds/954689156213113037';}
+	else if(goto=="spacesarchive"){window.location='https://account.altvr.com/worlds/1349925732620436463';}
+	else {window.location=window.location.origin+'/'+goto;}
+}
