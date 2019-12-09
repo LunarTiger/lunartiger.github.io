ab8dfd36cca4ebfdfa5f565042f8736f16b819e4
LunarTiger <lunartiger@live.com>
2019-12-09 04:20:44 -0500

    because efficiency

--- assets/js/music-embed.js
+++ assets/js/music-embed.js
@@ -6,12 +6,12 @@ var database = firebase.database();
 var state = database.ref('state');
 state.on('value', (function(snapshot) {
 	var stateVal = snapshot.val();
-	var id = database.ref('id');
-	id.on('value', (function(snapshot) {
-		var idVal = snapshot.val();
-		if(stateVal){
+	if(stateVal){
+		var id = database.ref('id');
+		id.on('value', (function(snapshot) {
+			var idVal = snapshot.val();
 			document.getElementById('musicembed').style = "max-width:100%;height:30px;width:350px;border: 0px"
 			document.getElementById('musicembed').src = "https://www.youtube.com/embed/"+idVal+"?rel=0;&autoplay=1";
-		}
-	}));
+		}));
+	}
 }));
