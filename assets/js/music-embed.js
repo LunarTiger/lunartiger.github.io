c0e7890e5f7cbee304af3471230f3bd1e0d339cb
LunarTiger <lunartiger@live.com>
2020-03-05 23:00:06 -0500

    :tada:

--- assets/js/music-embed.js
+++ assets/js/music-embed.js
@@ -10,7 +10,7 @@ state.on('value', (function(snapshot) {
 		var id = database.ref('id');
 		id.on('value', (function(snapshot) {
 			var idVal = snapshot.val();
-			document.getElementById('musicembed').style = "max-width:100%;height:30px;width:350px;border: 0px"
+			document.getElementById('musicembed').style = "max-width:100%;height:197px;width:350px;border: 0px"
 			document.getElementById('musicembed').src = "https://www.youtube.com/embed/"+idVal+"?rel=0;&autoplay=1";
 		}));
 	}
