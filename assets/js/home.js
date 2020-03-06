35f934981304c2a43c42839e994f76176392cb0b
LunarTiger <lunartiger@live.com>
2020-03-06 16:33:47 -0500

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,14 +8,14 @@ var toot = database.ref('mastodon/toot');
 toot.on('value', (function(snapshot) {
 	var tootVal = snapshot.val();
 	if(tootVal){
-		document.getElementById('tootembed').width = "400"
-		document.getElementById('tootembed').style = "max-width: 100%;height:187;border: 0"
-		document.getElementById('tootembed').src = "https://mas.to/@lunar/"+tootVal+"/embed";
+		document.getElementById('tootembed').width = "400";
+		document.getElementById('tootembed').style = "max-width: 100%;height:187;border: 0";
+		document.getElementById('tootembed').src = tootVal+"/embed";
 	}
 	if(!tootVal){
-		document.getElementById('tootembed').width = "0"
-		document.getElementById('tootembed').style = "max-width:0px;height:0px;border: 0"
-		document.getElementById('tootembed').src = "about:blank";
+		document.getElementById('tootembed').width = "0";
+		document.getElementById('tootembed').style = "max-width:0px;height:0px;border: 0";
+		document.getElementById('tootembed').src = "";
 	}
 }));
 //Music
@@ -23,11 +23,11 @@ var id = database.ref('music-embed/id');
 id.on('value', (function(snapshot) {
 	var idVal = snapshot.val();
 	if(idVal){
-		document.getElementById('musicembed').style = "max-width:100%;height:197px;width:350px;border: 0px"
+		document.getElementById('musicembed').style = "max-width:100%;height:197px;width:350px;border: 0px";
 		document.getElementById('musicembed').src = "https://www.youtube.com/embed/"+idVal+"?rel=0;&autoplay=1";
 	}
 	if(!idVal){
-		document.getElementById('musicembed').style = "max-width:0px;height:0px;width:0px;border: 0px"
+		document.getElementById('musicembed').style = "max-width:0px;height:0px;width:0px;border: 0px";
 		document.getElementById('musicembed').src = "";
 	}
 }));
