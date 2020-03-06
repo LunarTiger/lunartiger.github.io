231db950f177aed392f56e7be2daaf76ed2ca211
LunarTiger <lunartiger@live.com>
2020-03-06 03:35:05 -0500

    :tada:

---
+++ assets/js/toot.js
@@ -0,0 +1,19 @@
+var config = {
+	databaseURL: "https://toot-embed.firebaseio.com/",
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var toot = database.ref('toot');
+state.on('value', (function(snapshot) {
+	var tootVal = snapshot.val();
+	if(tootVal){
+		document.getElementById('tootembed').width = "400"
+		document.getElementById('tootembed').style = "max-width: 100%; border: 0"
+		document.getElementById('tootembed').src = "https://mas.to/@lunar/"+tootVal+"/embed";
+	}
+	if(!tootVal){
+		document.getElementById('tootembed').width = "0"
+		document.getElementById('tootembed').style = "max-width:0px;height:0px;border: 0"
+		document.getElementById('tootembed').src = "about:blank";
+	}
+}));
