88db3ffdfd56cb9f5ff2b79459fa11eb126dfbcc
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2023-05-27 22:37:47 +0000

    :tada:

--- assets/js/hash-redirect.js
+++ assets/js/hash-redirect.js
@@ -1,7 +1,7 @@
 if(window.location.hash){
 	var goto = window.location.hash.substr(1);
 	if(goto=="files"){window.location=window.location.origin+'/fs-up';}
-	else if(goto=="lunarverse"){window.location='https://account.altvr.com/worlds/954689156213113037';}
-	else if(goto=="spacesarchive"){window.location='https://account.altvr.com/worlds/1349925732620436463';}
+	else if(goto=="revolt"){window.location='https://rvlt.gg/QwvVQrEA';}
+	else if(goto=="tokipona"){window.location='https://rvlt.gg/tAjN6wwM';}
 	else {window.location=window.location.origin+'/'+goto;}
 }
