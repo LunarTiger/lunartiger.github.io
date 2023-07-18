a977c8002946aceb74fb69c455122d5c7c513925
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2023-07-18 22:39:53 +0000

    :tada:

--- contact/scene-addin.js
+++ contact/scene-addin.js
@@ -1,12 +1,13 @@
-let master_info = [preffered, secure, messaging, fediverse, social, gaming, payme, other];
-preffered = ["Discord: rev_lunar", "Telegram: @Rev_Lunar", "Fediverse: @lunar@hell.social", "SpaceHey: /rev_lunar", "Phone: 540-692-6899"];
-secure = ["Matrix: @rev_lunar:matrix.org", "Signal: 15406926899", "Telegram: @Rev_Lunar", "Wickr: lunartiger", "Wire: @rev_lunar"];
-messaging = ["Discord: rev_lunar" ,"Revolt: Lunatics", "Snapchat: rev_lunar"];
-fediverse = ["Mastodon: lunar@mas.to", "Pixelfed: lunar@gram.social", "Calckey: rev_lunar@hell.social"];
-social = ["Instagram: rev_lunar", "SpaceHey: /rev_lunar", "TikTok: rev_lunar", "Twitch: Rev_Lunar"];
-gaming = ["SideQuest: Rev. Lunar", "Steam: rev_lunar", "Uplay: lunartiger1965", "VRChat: Rev. Lunar"];
-payme = ["Cash App: $revlunar", "PayPal: lunartiger", "Revolut: revlunar", "Venmo: @rev_lunar"];
-other = ["Phone: 540-692-6899", "YouTube: @rev_lunar", "SoundCloud: Rev. Lunar", "Spotify:  Rev. Lunar", "GitHub: LunarTiger"];
+let contact_info = [
+  ["Discord: rev_lunar", "Telegram: @Rev_Lunar", "Fediverse: @lunar@hell.social", "SpaceHey: /rev_lunar", "Phone: 540-692-6899"], // preferred
+  ["Matrix: @rev_lunar:matrix.org", "Signal: 15406926899", "Telegram: @Rev_Lunar", "Wickr: lunartiger", "Wire: @rev_lunar"], // secure
+  ["Discord: rev_lunar" ,"Revolt: Lunatics", "Snapchat: rev_lunar"], // messaging
+  ["Mastodon: lunar@mas.to", "Pixelfed: lunar@gram.social", "Calckey: rev_lunar@hell.social"], // fediverse
+  ["Instagram: rev_lunar", "SpaceHey: /rev_lunar", "TikTok: rev_lunar", "Twitch: Rev_Lunar"], // social
+  ["SideQuest: Rev. Lunar", "Steam: rev_lunar", "Uplay: lunartiger1965", "VRChat: Rev. Lunar"], // gaming
+  ["Cash App: $revlunar", "PayPal: lunartiger", "Revolut: revlunar", "Venmo: @rev_lunar"], // payme,
+  ["Phone: 540-692-6899", "YouTube: @rev_lunar", "SoundCloud: Rev. Lunar", "Spotify:  Rev. Lunar", "GitHub: LunarTiger"] // other
+];
 
 // function to create a box and text
 createTablet(this_array, txt_color, box_color, position, rotation) {
@@ -43,7 +44,7 @@ stringifyArray(this_array) {
 // create a new entity to contain the contact stuff
 let contact = document.createElement('a-entity'); contact.id = 'contact-scene'; contact.setAttribute('position', '0 0 -10');
 // for each array in the master one create a tablet
-for (let i = 0; i < master_info.length; i++) {
-  contact.appendChild(createTablet(master_info[i], '#00be00', '#000', i+' 0.5 0', '0 0 0'));
+for (let i = 0; i < contact_info.length; i++) {
+  contact.appendChild(createTablet(contact_info[i], '#00be00', '#000', i+' 0.5 0', '0 0 0'));
 }
 document.querySelector('a-scene').appendChild(contact);
