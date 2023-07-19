96a7dfe6fd5e52aac31137213358b8124ebdbe83
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2023-07-19 21:56:42 +0000

    :tada:

--- assets/js/scene-addin.js
+++ assets/js/scene-addin.js
@@ -9,13 +9,11 @@ let contact_info = [
   ["Mastodon: lunar@mas.to", "Pixelfed: lunar@gram.social", "Calckey: rev_lunar@hell.social"], // fediverse
   ["Instagram: rev_lunar", "SpaceHey: /rev_lunar", "TikTok: rev_lunar", "Twitch: Rev_Lunar"], // social
   ["SideQuest: Rev. Lunar", "Steam: rev_lunar", "Uplay: lunartiger1965", "VRChat: Rev. Lunar"], // gaming
-  ["Cash App: $revlunar", "PayPal: lunartiger", "Revolut: revlunar", "Venmo: @rev_lunar"], // payme,
+  ["Cash App: $revlunar", "PayPal: lunartiger", "Revolut: revlunar", "Venmo: @rev_lunar"], // payme
   ["Phone: 540-692-6899", "YouTube: @rev_lunar", "SoundCloud: Rev. Lunar", "Spotify:  Rev. Lunar", "GitHub: LunarTiger"] // other
 ];
 let home_info = [];
-let projects_info = [
-  ["placeholder", "info", "projects"]
-];
+let projects_info = [];
 let project_spaces = ["https://lunar-test.glitch.me"]
 let font_face = "https://lunartiger.github.io/assets/fonts/comic_sans/comic.ttf"
 
@@ -38,7 +36,7 @@ async function createTablet(this_array, txt_color, box_color, position, rotation
   text.setAttribute('color', txt_color);
   text.setAttribute('position', '0 0.03 0.06');
   text.setAttribute('value', value);
-  text.setAttribute('font', font_face);
+  // text.setAttribute('font', font_face); // font faces don't work in Banter :'(
   text.setAttribute('scale', '0.5 1 0.5');
   text.setAttribute('align', 'center');
   // parent the text to the box and return the box
@@ -54,9 +52,9 @@ async function stringifyArray(this_array) {
   return this_string;
 }
 
-async function orderTablets(position, this_info) {
+async function orderTablets(position, rotation, group_id, this_info) {
   // create a new entity to contain the stuff
-  let this_scene = document.createElement('a-entity'); this_scene.id = 'this-scene'; this_scene.setAttribute('position', position);
+  let this_scene = document.createElement('a-entity'); this_scene.id = group_id; this_scene.setAttribute('position', position); this_scene.setAttribute('rotation', "0 "+rotation+" 0");
   // for each array in the master one create a tablet
   for (let i = 0; i < this_info.length; i++) {
     let tablet = await createTablet(this_info[i], '#00be00', '#000', (i*1.5)+' 0.055 0', '270 0 0');
@@ -81,20 +79,21 @@ document.querySelector('a-scene').appendChild(this_entity);
 window.onload = () => {
   // let page_location = window.location.pathname;
   if(window.location.pathname == "/about") {
-    orderTablets("-2 0.055 -10", about_info);
+    orderTablets("-2 0.055 -10", "0", "about-info", about_info);
   }
   else if(window.location.pathname == "/contact") {
-    orderTablets("-5 0.055 -10", contact_info);
+    orderTablets("-5 0.055 -10", "0", "contact_info", contact_info);
   }
   else if(window.location.pathname == "/projects") {
-    orderTablets("0 0.055 -10", projects_info);
-    projectPortals("18.57 0.3 18.57", "0 270 0", project_spaces);
+    orderTablets("0 0.055 -10", "0", "projects-info", projects_info);
+    projectPortals("18.57 0.3 18.57", "0", "0 270 0", project_spaces);
   }
   else if(window.location.pathname == "/") {
-   orderTablets("0 0.055 -10", home_info);
+   orderTablets("0 0.055 -10", "0", "home-info", home_info);
   }
   else {
-    orderTablets("-2 0.055 -10", [["warning:", "space not found or not made"], ["Hail Satan!"]]);
+    orderTablets("0 0.055 -10", "0", "warning-info", [["warning:", "space not found or not made"]]);
   }
-  orderTablets("0 0.055 10", [["window.location.pathname", window.location.pathname]])
+  orderTablets("-10 0 0", "90", "hail-satan", [["Hail Satan!"]])
+  orderTablets("0 0.055 10", "180", "window-pathname" [["window.location.pathname", window.location.pathname]])
 }
