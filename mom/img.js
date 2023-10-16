d7048b8ccb200b3a6f2589c993ff10925a462f20
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2023-10-16 01:36:08 +0000

    :tada:

--- mom/img.js
+++ mom/img.js
@@ -1,14 +1,10 @@
 addEventListener("DOMContentLoaded", async() => {
-	const img = await fetch('img.json');
-	const html = document.createElement("p");
+	const raw_img = await fetch('img.json');
+	const img = await raw_img.json();
 	img.forEach((d, i) => {
 		const this_img = document.createElement("img");
 		this_img.alt = d.alt;
 		this_img.src = "img/"+d.file;
-		this_img.appendChild(html);
+		document.getElementById("img").appendChild(this_img);
 	});
-	document.getElementById("img").innerHTML = html;
-	for (l=0; l<alt.length; l++){
-		document.body.innerHTML += "<p><img src='img/"+l+".jpg' alt='"+alt[l]+"'></img><br />"+alt[l]+"</p><hr style='height:5px; visibility:hidden;' />";
-	}
 });
