e19ac14dcf9346f38b99bc047a9408102e689702
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2024-04-25 22:24:47 +0000

    :tada:

--- assets/js/hash-redirect.js
+++ assets/js/hash-redirect.js
@@ -1,6 +1,8 @@
 if(window.location.hash){
 	const goto = window.location.hash.substr(1);
-	if(goto=="files"){window.location.replace(window.location.origin+'/fs-up');}
-	else if(goto=="revolt"){window.location.replace('https://rvlt.gg/QwvVQrEA');}
-	else {window.location.replace(window.location.origin+'/'+goto);}
+	switch(goto){
+		case 'files' : window.location.replace(window.location.origin+'/fs-up'); break;
+		case 'revolt' : window.location.replace('https://rvlt.gg/QwvVQrEA'); break;
+		default : window.location.replace(window.location.origin+'/'+goto);
+	}
 }
