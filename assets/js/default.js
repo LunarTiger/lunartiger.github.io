e2d73671744358ad9e7c24535bed878d21a9a707
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2024-05-26 13:44:54 +0000

    :tada:

---
+++ assets/js/default.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded", () => {
+  const quotesEl = document.getElementById('quotesgohere'), quotesurl="https://lunartiger.github.io/quotes.json";
+  if(quotes){
+    try{
+      const quotes = await fetch(quotesurl);
+      const quotesJson = await links.json();
+      for(let i = 0; i < quotesJson.length; i++){
+        quotesEl.innerHTML += "<hr />"+quotesJson[i];
+      };
+    }catch{
+      quotesEl.innerHTML += "<mark>Loading failed :'(</mark>";
+    };
+  };
+});
