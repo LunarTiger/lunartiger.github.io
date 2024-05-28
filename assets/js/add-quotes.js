204d67b83974311cd8aec129c360900e2e3e8551
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2024-05-28 19:50:48 +0000

    :tada:

--- assets/js/add-quotes.js
+++ assets/js/add-quotes.js
@@ -4,18 +4,18 @@ addEventListener("DOMContentLoaded", async() => {
     try{
       const quotes = await fetch(quotesURL);
       const quotesJson = await quotes.json();
-      if((quotesEl.nodeName == "DETAILS") && quotesJson){ // display the whole list
-        for(let i = 0; i < quotesJson.length; i++){
-          if(window.location.hostname == 'lunar.bant.ing'){quotesJson[i] = quotesJson[i].toLowerCase()};
-          quotesEl.innerHTML += "<hr />"+quotesJson[i];
-        };
-        if(window.location.hostname == 'lunar.bant.ing'){quotesEl.innerHTML += "<hr />";};
-      }else if(quotesJson){ // randomly choose a quote from the list and display it
-        quotesEl.innerHTML = "<p>"+quotesJson[Math.round(Math.random()*(quotesJson.length-1))]+"</p>";
-      }else{ // hide the element if it failed to fetch the list
-        quotesEl.style = "display:none;";
-      };
     }catch{
+      const quotesJson = null;
+    };
+    if((quotesEl.nodeName == "DETAILS") && quotesJson){ // display the whole list
+      for(let i = 0; i < quotesJson.length; i++){
+        if(window.location.hostname == 'lunar.bant.ing'){quotesJson[i] = quotesJson[i].toLowerCase()};
+        quotesEl.innerHTML += "<hr />"+quotesJson[i];
+      };
+      if(window.location.hostname == 'lunar.bant.ing'){quotesEl.innerHTML += "<hr />";};
+    }else if(quotesJson){ // randomly choose a quote from the list and display it
+      quotesEl.innerHTML = "<p>"+quotesJson[Math.round(Math.random()*(quotesJson.length-1))]+"</p>";
+    }else{ // hide the element if it failed to fetch the list
       quotesEl.style = "display:none;";
     };
   };
