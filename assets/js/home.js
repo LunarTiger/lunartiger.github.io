e37b623a501742cb9cd5d3888668d7eda344b362
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2024-12-18 18:46:48 +0000

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -99,7 +99,7 @@ id.on('value', (function(snapshot) {
 	var idVal = snapshot.val();
 	if(idVal && !!document.getElementById('musicembed')){
 		document.getElementById('musicembed').style = "max-width:100%;height:197px;width:350px;border: 0px";
-		document.getElementById('musicembed').src = "https://www.youtube.com/embed/"+idVal+"?rel=0;&autoplay=0";
+		document.getElementById('musicembed').src = "https://www.youtube-nocookie.com/embed/"+idVal+"?rel=0;&autoplay=0";
 	}
 	if(!idVal && !!document.getElementById('musicembed')){
 		document.getElementById('musicembed').style = "max-width:0px;height:0px;width:0px;border: 0px";
